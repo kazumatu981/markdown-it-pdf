d93539afa1e87e4c476936c628591001bfa3d1fb
kazumatu981 <kazumatu981@gmail.com>
2024-05-02 10:39:43 +0900

    second prototype of  `puppeteer-pdf-printer`.

--- src/core/puppeteer-pdf-prinrer.ts
+++ src/core/puppeteer-pdf-prinrer.ts
@@ -1,28 +1,70 @@
+import { PDFOptions, type PaperFormat } from 'puppeteer';
 import puppeteer from 'puppeteer';
 import { buildTreeOfFiles } from './path-resolver';
+import path from 'path';
+
+export type PuppeteerPDFPrinterPaperFormat = Omit<PaperFormat, 'path'>;
+
 export async function printManyPages(
-    site: string,
-    pages: string[],
-    outputPath: string
+    siteUrl: string,
+    pagePaths: string[],
+    outputDir: string,
+    paperFormat?: PuppeteerPDFPrinterPaperFormat
 ) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    // build folder tree.
+    await buildTreeOfFiles(pagePaths.map((page) => path.join(outputDir, page)));
 
-    await buildTreeOfFiles(pages.map((page) => `${outputPath}/${page}`));
-
-    const urls = pages.map((page) => {
+    const urls = pagePaths.map((page) => {
         return {
-            url: page,
-            fullUrl: `${site}/${page}`,
+            pathToPdf: `${path.join(outputDir, page)}.pdf`,
+            fullUrl: `${siteUrl}/${page}`,
         };
     });
+
+    // launch puppeteer
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
     for (const url of urls) {
+        // go to page
         await page.goto(url.fullUrl);
+        // wait for the page to be loaded.
+        await page.waitForSelector('body');
+        // print into pdf file
         await page.pdf({
-            path: `${outputPath}/${url.url}.pdf`,
-            format: 'a4',
-            landscape: true,
+            path: url.pathToPdf,
+            ...paperFormat,
         });
     }
-    browser.close();
+    // close page and browser
+    await page.close();
+    await browser.close();
+    return;
+}
+
+export async function printOnePage(
+    siteUrl: string,
+    pagePath: string,
+    outputDir: string,
+    paperFormat?: PuppeteerPDFPrinterPaperFormat
+) {
+    await printManyPages(siteUrl, [pagePath], outputDir, paperFormat);
+    return;
+}
+
+export async function printIntoMemory(
+    siteUrl: string,
+    pagePath: string,
+    paperFormat?: PuppeteerPDFPrinterPaperFormat
+): Promise<Buffer> {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    // go to page
+    await page.goto(`${siteUrl}/${pagePath}`);
+    // wait for the page to be loaded.
+    await page.waitForSelector('body');
+    // print into pdf file
+    const pdf = await page.pdf(paperFormat as PDFOptions);
+    await page.close();
+    await browser.close();
+    return pdf;
 }
