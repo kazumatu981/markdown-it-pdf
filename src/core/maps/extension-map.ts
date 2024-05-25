e722cb38e2f447ab7a28e9a9834fd4aec01eac1e
kazumatu981 <kazumatu981@gmail.com>
2024-05-26 07:12:52 +0900

    add jsdocs.

--- src/core/maps/extension-map.ts
+++ src/core/maps/extension-map.ts
@@ -7,10 +7,24 @@ interface ExtensionTypeInfo {
     resolvedContentType?: string;
 }
 
+/**
+ * A class that maps file extensions to their associated resolver types and content types.
+ */
 export class ExtensionMap extends Map<string, ExtensionTypeInfo> {
+    /**
+     * Resolve the type info for an extension.
+     * @param extName - The extension name.
+     * @returns {ExtensionTypeInfo} Information about the extension (resolver type, content type).
+     */
     public getTypeInfo(extName: string): ExtensionTypeInfo {
         return this.get(extName) ?? defaultTypeInfo;
     }
+
+    /**
+     * Test the file name against the list of supported extensions.
+     * @param {string} fileName - The file name to check.
+     * @returns {boolean} True if the file name is supported, false otherwise.
+     */
     public isSupported(fileName: string): boolean {
         return this.has(path.extname(fileName));
     }
