97dabb558a3ecd32c781331fb90092e4ba186beb
kazumatu981 <kazumatu981@gmail.com>
2024-06-13 18:18:22 +0900

    rule for metrics.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,8 +6,17 @@ export default [
     { languageOptions: { globals: globals.node } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
+    // rules for code metrics
     {
         rules: {
+            'max-lines-per-function': ['error', 300],
+            'max-lines': ['error', 300],
+            'max-len': ['error', { code: 120, ignoreComments: true }],
+            'max-statements-per-line': ['error', { max: 1 }],
+            'max-statements': ['error', 21],
+            'max-nested-callbacks': ['error', 3],
+            'max-params': ['error', 3],
+            'max-depth': ['error', 3],
             complexity: ['error', 10],
         },
     },
