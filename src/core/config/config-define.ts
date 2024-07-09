21716ea847db3bd13d693f98955bb4f6ced589cd
Kazuyoshi Matsumoto <kazumatu981@gmail.com>
2024-07-09 17:50:57 +0900

    just backup.

--- src/core/config/config-define.ts
+++ src/core/config/config-define.ts
@@ -30,3 +30,46 @@ export type CategorizedConfigDefines = Record<
     ConfigCategory,
     CategorizedConfigDefine
 >;
+
+export type PropertyValueFormatter = (value: PropertyValue) => Array<string>;
+
+const defaultValueFormatter: PropertyValueFormatter = (value) => {
+    return [`${value}`];
+};
+
+const defaultArrayFormatter: PropertyValueFormatter = (value) => {
+    return [
+        '[',
+        ...(value as Array<string | number>).map((item) => `    ${item}`),
+        ']',
+    ];
+};
+
+const objectFormatter: PropertyValueFormatter = (value) => {
+    throw new Error('Not implemented');
+};
+const valueFormatterMap: Record<PropertyValueType, PropertyValueFormatter> = {
+    string: defaultValueFormatter,
+    number: defaultValueFormatter,
+    boolean: defaultValueFormatter,
+    'string-array': defaultArrayFormatter,
+    'number-array': defaultArrayFormatter,
+    object: objectFormatter,
+};
+
+function formatPropertyDefine(
+    key: string,
+    configDefine: PropertyDefine
+): Array<string> {
+    const formatter = valueFormatterMap[configDefine.type];
+    throw new Error('Not implemented');
+}
+
+function formatConfigDefine(
+    configDefine: Record<string, PropertyDefine>
+): Array<string> {
+    const properties = Object.keys(configDefine)
+        .map((key) => formatPropertyDefine(key, configDefine[key]))
+        .flat();
+    return ['{', ...properties, '}'];
+}
