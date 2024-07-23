f8fdfd3b04c41cfa19002fb4328fda3492762561
kazumatu981 <kazumatu981@gmail.com>
2024-07-24 07:56:39 +0900

    commented.

--- src/core/config/config-define-types.ts
+++ src/core/config/config-define-types.ts
@@ -1,3 +1,6 @@
+/**
+ * The value type of a config property
+ */
 export type PropertyValue =
     | undefined
     | string
@@ -6,6 +9,10 @@ export type PropertyValue =
     | Record<string, PropertyDefine>
     | Array<string>
     | Array<number>;
+
+/**
+ * The value type name of a config property
+ */
 export type PropertyValueType =
     | 'undefined'
     | 'string'
@@ -14,18 +21,49 @@ export type PropertyValueType =
     | 'string-array'
     | 'number-array'
     | 'object';
+
+/**
+ * The definition of a config property
+ */
 export interface PropertyDefine {
+    /**
+     * The description of the property
+     */
     description?: Array<string>;
+    /**
+     * The value type of the property
+     */
     type: PropertyValueType;
+    /**
+     * Whether the property is commented
+     */
     isCommented?: boolean;
+    /**
+     * The value of the property
+     */
     value: PropertyValue;
 }
 
+/**
+ * The category of a config property
+ */
 export type ConfigCategory = 'render' | 'server' | 'printer';
 
+/**
+ * a categorized config object
+ */
 export interface CategorizedConfig {
+    /**
+     * The description of the configurations
+     */
     description: Array<string>;
+    /**
+     * config properties
+     */
     properties: Record<string, PropertyDefine>;
 }
 
+/**
+ * The config object
+ */
 export type ConfigDefines = Record<ConfigCategory, CategorizedConfig>;
